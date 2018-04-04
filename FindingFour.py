338e5350f3dc9b8d25abb15b42799a4744f6d03f
Andrew Folken <folkenandrew@gmail.com>
2018-04-04 13:39:15 -0600

    FindFourAdvanced
    Rewrote code more concisely.

--- FindingFour.py
+++ FindingFour.py
@@ -1,28 +1,21 @@
 
 y = int(input('Enter Number: '))
 
-for i in range(0,10):
-    print(2**i)
 
 def FindFour(x):
-
-    if x > 512:
-        x = x - 512
-    if x > 256:
-        x = x - 256
-    if x > 128:
-        x = x - 128
-    if x > 64:
-        x = x - 64
-    if x > 32:
-        x = x - 32
-    if x > 16:
-        x = x - 16
-    if x > 8:
-        x = x - 8
+    i = 9
+    while i > 1:
+        if x > 2**i:
+            x = x - 2**i
+        #print(x)
+        if x <= 8:
+            break
+        i=i-1
+        
     if x < 8 and x >= 4:
         print('Y')
     else:
         print('N')
-
+        
 FindFour(y)
+
